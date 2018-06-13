3657bc66eceeca7c62d4ffd08416b6e61ec60a87
Dave Beck <dbeck2@outlook.com>
2018-06-13 17:43:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,9 @@ var playlist = {
   cardiB: "I Like It",
   postMalone: "Psycho",
 }
+function updatePlaylist(playlist, "lilBaby", "Yes Indeed") {
+  return playlist.assign({}, playlist, {[lilBaby: "Yes Indeed"]});
+}
+function removeFromPlaylist("drake", "Nice for What") {
+  return delete playlist.drake;
+}
