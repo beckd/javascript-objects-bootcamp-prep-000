5c76d7811ccf2bae71107a8b9cb3989898dc36bf
Dave Beck <dbeck2@outlook.com>
2018-06-13 18:25:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,12 @@ var playlist = {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
 };
+function updatePlaylist(playlist, 'Phil Ochs', songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
