d89a761ccf8e23fdc591b79a2214739af2113dee
Dave Beck <dbeck2@outlook.com>
2018-06-13 18:12:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,3 @@ var playlist = {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
 };
-function removeFromPlaylist(playlist, 'Slowdive') {
-  delete playlist[Slowdive];
-  return playlist;
-}
