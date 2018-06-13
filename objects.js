3bf7e252e452cd426cb4f1537912356adbda93b1
Dave Beck <dbeck2@outlook.com>
2018-06-13 18:20:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist = {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
 };
+function updatePlaylist(playlist) {
+  playlist[’Phil Ochs'] = 'Here's to the State of Mississippi'
+}
