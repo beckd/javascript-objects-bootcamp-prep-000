314a3468594e8ce34dbbf0108b0eb58488c3645e
Dave Beck <dbeck2@outlook.com>
2018-06-13 18:35:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
-
 function removeFromPlaylist(playlist, Slowdive) {
   delete playlist[Slowdive];
   return playlist;
