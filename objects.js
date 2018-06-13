27807bbbf3711415801c3a82d7c9039acabae0ff
Dave Beck <dbeck2@outlook.com>
2018-06-13 17:50:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   cardiB: "I Like It",
   postMalone: "Psycho",
 }
-function updatePlaylist(playlist, lilBaby, Yes Indeed) {
+function updatePlaylist(playlist) {
   playlist.lilBaby = 'Yes Indeed';
   return playlist
 }
